8d9e77bbd64bb0b09a6d58ad2395b488057b4ce7
nishant@pipecandy.com <nishant@pipecandy.com>
2023-01-24 07:41:50 +0000

    Added sources

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -5,7 +5,8 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ source('jaffle_shop', 'customers') }}
+    -- schema and tablename
 
 )
 

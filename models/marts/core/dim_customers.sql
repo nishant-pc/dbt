c54770b3a670a5a97bfadbe9b90ff267c6ef2562
nishant@pipecandy.com <nishant@pipecandy.com>
2023-01-24 07:19:09 +0000

    Added payment model

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,7 +3,7 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
@@ -13,8 +13,8 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
 
     group by 1
@@ -29,8 +29,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
     from customers
 
     left join customer_orders using (customer_id)

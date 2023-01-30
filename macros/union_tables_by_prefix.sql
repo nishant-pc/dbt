668d62707d9df8df163ca3bd7c728943cef30efd
nishant@pipecandy.com <nishant@pipecandy.com>
2023-01-30 12:35:42 +0000

    advanced macros

---
+++ macros/union_tables_by_prefix.sql
@@ -0,0 +1,16 @@
+{%- macro union_tables_by_prefix(database, schema, prefix) -%}
+
+  {%- set tables = dbt_utils.get_relations_by_prefix(database=database, schema=schema, prefix=prefix) -%}
+
+  {{ log('Result is ' ~ tables)}}
+  {% for table in tables %}
+
+      {%- if not loop.first -%}
+      union all 
+      {%- endif %}
+        
+      select * from {{ table.database }}.{{ table.schema }}.{{ table.name }}
+      
+  {% endfor -%}
+  
+{%- endmacro -%}
---
+++ models/union_tables_by_orders_prefix .sql
@@ -0,0 +1,8 @@
+{{ union_tables_by_prefix(
+
+      database='raw',
+      schema='jaffle_shop', 
+      prefix='custom'
+        
+      )
+}}
